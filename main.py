f263aed61c77082b613a7c948cdc8bdc8c6e8a2e
Alex Yoon <alex04.yoon@gmail.com>
2026-09-17 22:36:26 -0700

    post paths

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+class Session(BaseModel): #basemodel is a  class from pydantic that knows how to parse incoming json. Session is a child inheriting form basemodel
+   date: str # no fallback values, so required fields that dont have a fallback value and give error instead
+   location: str
+   stakes: str
+   buy_in: float
+   cash_out: float
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"status": "ok"}
 
-@app.get("/first")
-def read_first():
-    return {"message": "This is the first endpoint."}
+@app.get("/sessions/{session_id}")
+def read_session(session_id: int):
+    return {"session_id": session_id}
+
+@app.get("/sessions")
+def read_sessions(limit: int = 10):
+    return {"limit": limit}
+
+@app.post("/sessions")
+def sessions(session: Session):
+    return {"session": session}
